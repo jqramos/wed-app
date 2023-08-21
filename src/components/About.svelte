9295ac85b147b4a5573cdcbfbf3a4f759b1c0037
Janica Chioco <janicachioco@gmail.com>
2023-08-21 18:48:45 +0800

    # Update folder structure # Update tailwind config # Add Hero, About components

---
+++ src/components/About.svelte
@@ -0,0 +1,6 @@
+
+<section>
+    <div class="wrapper">
+        About
+    </div>
+</section>
---
+++ src/components/Hero.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Logo from '../atoms/Logo.svelte';
+</script>
+
+
+<section class="relative h-screen text-white overflow-hidden before:absolute before:h-full before:w-full before:bg-gradient1 before:inset-0" style="background: url(../../static/images/image-1.webp) no-repeat cover;">
+    <div class="h-full w-full">
+        <img class="h-full w-full object-cover" src="../../static/images/image-5.webp" alt="">
+    </div>
+    <div class="absolute inset-0">
+        <div class="wrapper h-full">
+            <div class="col-span-full flex justify-center items-center flex-col">
+                <Logo />
+                <h1 class="font-serif text-h1 uppercase text-center">Jayvee & Victoria</h1>
+                <h3 class="font-serif text-h4 uppercase text-center">The Wedding</h3>
+            </div>
+        </div>
+    </div>
+</section>
