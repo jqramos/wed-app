56ac8c0513303e1faa7acb2bb37563842a1f3b7b
Janica Chioco <janicachioco@gmail.com>
2023-08-22 02:42:53 +0800

    [FE]: FAQ Module

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,7 +1,8 @@
 <script>
     import Layout from '../utils/Layout.svelte';
     import Hero from '../components/Hero.svelte';
+    import FAQ from '../components/FAQ.svelte';
     import About from '../components/About.svelte';
 </script>
 
-<Layout components={[Hero, About]} />
+<Layout components={[Hero, FAQ, About]} />
