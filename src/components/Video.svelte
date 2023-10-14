fc413094d19e16d818ab0504deb60354a9cb07c5
jvqramos <jvqramos@gmail.com>
2023-10-14 10:25:07 +0800

    allow fullscreen

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -1,5 +1,5 @@
 <section class="panel js-panel relative h-screen text-white overflow-hidden">
     <div class="h-full w-full">
-        <iframe src="https://drive.google.com/file/d/12SCM16dxWrQvXJBprTocX5ecjhpdOlrp/preview" width="100%" height="100%" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/12SCM16dxWrQvXJBprTocX5ecjhpdOlrp/preview" width="100%" height="100%" allow="autoplay" allowFullScreen="allowFullScreen" frameBorder="0"></iframe>
     </div>
 </section>
